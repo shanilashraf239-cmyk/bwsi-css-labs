60ba0c0af6ce169a49c178a4d63482f3c0f80ae5
shanilashraf239-cmyk <shanilashraf239@gmail.com>
2026-03-22 21:23:29 -0400

    new my intro print

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "shanil ashraf" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I like playing soccer w my friends")
 
 if __name__ == "__main__":
     main()
